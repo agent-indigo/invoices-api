5a66b7a79c451c24a8f75c8f0fa0bcc3c7d0936d
agent-indigo <bradenhynes@gmail.com>
2024-07-12 18:41:49 -0230

    Added App constructor

--- src/main/java/com/github/agent_indigo/invoices_api/classes/App.java
+++ src/main/java/com/github/agent_indigo/invoices_api/classes/App.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
     "com.github.agent_indigo.invoices_api.interfaces.repositores"
 })
 public class App {
+    public App() {}
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
